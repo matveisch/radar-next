718a49749cad84533fb4f45e2aa33d9dc33d4590
Matvei <matvesch@protonmail.com>
2022-11-17 15:21:09 +0200

    added radar component

---
+++ components/radar/Radar.tsx
@@ -0,0 +1,171 @@
+import { motion, MotionConfig } from "framer-motion";
+import Image from "next/image";
+import chain from "../../images/chain.svg";
+import connection from "../../images/connection.svg";
+import facebook from "../../images/facebook.svg";
+import graph from "../../images/graph.svg";
+import instagram from "../../images/instagram.svg";
+import messenger from "../../images/messenger.svg";
+import radar from "../../images/radar.svg";
+import social from "../../images/social.svg";
+import sound from "../../images/sound.svg";
+import stat from "../../images/stat.svg";
+import telegram from "../../images/telegram.svg";
+import style from "./Radar.module.scss";
+
+export default function Radar() {
+  interface motionDivProps {
+    source: string;
+    id: string;
+
+    coordinates: {
+      x: string;
+      y: string;
+    };
+    duration: number;
+    delay?: number;
+  }
+
+  const iconsArr: Array<motionDivProps> = [
+    {
+      source: sound,
+      id: style.sound,
+      coordinates: { x: "1500%", y: "700%" },
+      duration: 7,
+    },
+    {
+      source: graph,
+      id: style.graph,
+      coordinates: { x: "1000%", y: "1200%" },
+      duration: 5,
+      delay: 0.2,
+    },
+    {
+      source: facebook,
+      id: style.facebook,
+      coordinates: { x: "1100%", y: "1200%" },
+      duration: 10,
+      delay: 0.4,
+    },
+    {
+      source: connection,
+      id: style.connection,
+      coordinates: { x: "400%", y: "1200%" },
+      duration: 5,
+      delay: 0.6,
+    },
+    {
+      source: messenger,
+      id: style.messenger,
+      coordinates: { x: "-100%", y: "1200%" },
+      duration: 8,
+      delay: 0.8,
+    },
+    {
+      source: social,
+      id: style.social,
+      coordinates: { x: "-500%", y: "1200%" },
+      duration: 9,
+      delay: 1,
+    },
+    {
+      source: chain,
+      id: style.chain,
+      coordinates: { x: "-1100%", y: "1200%" },
+      duration: 6,
+      delay: 1.8,
+    },
+    {
+      source: telegram,
+      id: style.telegram,
+      coordinates: { x: "-1300%", y: "1200%" },
+      duration: 12,
+    },
+    {
+      source: stat,
+      id: style.stat,
+      coordinates: { x: "-1400%", y: "500%" },
+      duration: 11,
+      delay: 2,
+    },
+  ];
+
+  function MotionDiv({
+    source,
+    id,
+    coordinates,
+    duration,
+    delay,
+  }: motionDivProps) {
+    return (
+      <motion.div
+        animate={coordinates}
+        transition={{
+          ease: "linear",
+          duration: duration,
+          repeat: Infinity,
+          repeatDelay: 0,
+          delay: delay,
+        }}
+        id={id}
+        className={style.imageWrapper}
+      >
+        <Image src={source} alt="" />
+      </motion.div>
+    );
+  }
+
+  return (
+    <div id={style.crop}>
+      <div id={style.radarContainer}>
+        <div id={style.titles}>
+          <h2 className="H3">full-cycle</h2>
+          <h1 className="H1">Digital Marketing Agency</h1>
+        </div>
+
+        <div id={style.background}>
+          {iconsArr.map((icon, index) => {
+            return (
+              <MotionDiv
+                key={index}
+                source={icon.source}
+                id={icon.id}
+                coordinates={icon.coordinates}
+                duration={icon.duration}
+                delay={icon.delay}
+              />
+            );
+          })}
+        </div>
+        <div id={style.radarParent}>
+          <motion.div
+            id={style.radar}
+            animate={{
+              rotate: -270,
+              opacity: 0.3,
+            }}
+            transition={{
+              default: {
+                ease: "linear",
+                repeat: Infinity,
+                duration: 8,
+                repeatDelay: 1,
+              },
+              opacity: {
+                ease: "linear",
+                repeat: Infinity,
+                duration: 0.8,
+                repeatType: "reverse",
+              },
+              background: {
+                repeat: Infinity,
+                duration: 5,
+                repeatType: "reverse",
+              },
+            }}
+          ></motion.div>
+        </div>
+      </div>
+    </div>
+  );
+}
