4e96d7be28452ad918689bca1b72793613f4a67a
Lidkovsky <armkudr@gmail.com>
2022-11-29 19:45:35 +0500

    changes

--- components/attendance-meter/AttendanceMeter.tsx
+++ components/attendance-meter/AttendanceMeter.tsx
@@ -7,78 +7,84 @@ interface Props {
 
 function AttendanceMeter({ value }: Props) {
   return (
-    <div id={styles.borderAround}>
-      <div id={styles.mainWrapper}>
-        {/* TRIANGLES */}
-        <motion.div
-          style={{ left: "10%", top: "40%" }}
-          animate={{ opacity: value > 70 ? value / 100 : 0, rotate: 30 }}
-          className={styles.triangle}
-        ></motion.div>
-        <motion.div
-          style={{ left: "40%", top: "85%" }}
-          animate={{ opacity: value > 25 ? value / 100 : 0, rotate: 200 }}
-          className={styles.triangle}
-        ></motion.div>
-        {/* <motion.div
+    <>
+      <div id={styles.borderAround}>
+        <div id={styles.mainWrapper}>
+          <div id={styles.bgMarkup}></div>
+          {/* TRIANGLES */}
+          <motion.div
+            style={{ left: "10%", top: "40%" }}
+            animate={{ opacity: value > 70 ? value / 100 : 0, rotate: 30 }}
+            className={styles.triangle}
+          ></motion.div>
+          <motion.div
+            style={{ left: "40%", top: "85%" }}
+            animate={{ opacity: value > 25 ? value / 100 : 0, rotate: 200 }}
+            className={styles.triangle}
+          ></motion.div>
+          {/* <motion.div
           style={{ left: "70%", top: "50%" }}
           animate={{ opacity: value > 60 ? value / 100 : 0, rotate: 250 }}
           className={styles.triangle}
         ></motion.div> */}
-        <motion.div
-          style={{ left: "70%", top: "35%" }}
-          animate={{ opacity: value > 0 ? value / 100 : 0, rotate: 250 }}
-          className={styles.triangle}
-        ></motion.div>
-        {/* RECTANGLES */}
-        <motion.div
-          style={{ left: "70%", top: "5%" }}
-          animate={{ opacity: value > 65 ? value / 100 : 0, rotate: 20 }}
-          className={styles.rectangle}
-        ></motion.div>
-        <motion.div
-          style={{ left: "15%", top: "80%" }}
-          animate={{ opacity: value > 30 ? value / 100 : 0, rotate: 80 }}
-          className={styles.rectangle}
-        ></motion.div>
-        <motion.div
-          style={{ left: "60%", top: "70%" }}
-          animate={{ opacity: value > 80 ? value / 100 : 0, rotate: 45 }}
-          className={styles.rectangle}
-        ></motion.div>
-        <motion.div
-          style={{ left: "20%", top: "20%" }}
-          animate={{ opacity: value > 10 ? value / 100 : 0 }}
-          className={styles.rectangle}
-        ></motion.div>
-        {/* CIRCLES */}
-        <motion.div
-          style={{ left: "20%", top: "20%" }}
-          animate={{ opacity: value > 60 ? value / 100 : 0 }}
-          className={styles.circle}
-        ></motion.div>
-        <motion.div
-          style={{ left: "50%", top: "15%" }}
-          animate={{ opacity: value > 70 ? value / 100 : 0 }}
-          className={styles.circle}
-        ></motion.div>
-        <motion.div
-          style={{ left: "30%", top: "55%" }}
-          animate={{ opacity: value > 35 ? value / 100 : 0 }}
-          className={styles.circle}
-        ></motion.div>
-        <motion.div
-          style={{ left: "80%", top: "80%" }}
-          animate={{ opacity: value > 5 ? value / 100 : 0 }}
-          className={styles.circle}
-        ></motion.div>
-        <motion.div
-          style={{ left: "90%", top: "30%" }}
-          animate={{ opacity: value > 40 ? value / 100 : 0 }}
-          className={styles.circle}
-        ></motion.div>
+          <motion.div
+            style={{ left: "70%", top: "35%" }}
+            animate={{ opacity: value > 0 ? value / 100 : 0, rotate: 250 }}
+            className={styles.triangle}
+          ></motion.div>
+          {/* RECTANGLES */}
+          <motion.div
+            style={{ left: "70%", top: "5%" }}
+            animate={{ opacity: value > 65 ? value / 100 : 0, rotate: 20 }}
+            className={styles.rectangle}
+          ></motion.div>
+          <motion.div
+            style={{ left: "15%", top: "80%" }}
+            animate={{ opacity: value > 30 ? value / 100 : 0, rotate: 80 }}
+            className={styles.rectangle}
+          ></motion.div>
+          <motion.div
+            style={{ left: "60%", top: "70%" }}
+            animate={{ opacity: value > 80 ? value / 100 : 0, rotate: 45 }}
+            className={styles.rectangle}
+          ></motion.div>
+          <motion.div
+            style={{ left: "20%", top: "20%" }}
+            animate={{ opacity: value > 10 ? value / 100 : 0 }}
+            className={styles.rectangle}
+          ></motion.div>
+          {/* CIRCLES */}
+          <motion.div
+            style={{ left: "20%", top: "20%" }}
+            animate={{ opacity: value > 60 ? value / 100 : 0 }}
+            className={styles.circle}
+          ></motion.div>
+          <motion.div
+            style={{ left: "50%", top: "15%" }}
+            animate={{ opacity: value > 70 ? value / 100 : 0 }}
+            className={styles.circle}
+          ></motion.div>
+          <motion.div
+            style={{ left: "30%", top: "55%" }}
+            animate={{ opacity: value > 35 ? value / 100 : 0 }}
+            className={styles.circle}
+          ></motion.div>
+          <motion.div
+            style={{ left: "80%", top: "80%" }}
+            animate={{ opacity: value > 5 ? value / 100 : 0 }}
+            className={styles.circle}
+          ></motion.div>
+          <motion.div
+            style={{ left: "90%", top: "30%" }}
+            animate={{ opacity: value > 40 ? value / 100 : 0 }}
+            className={styles.circle}
+          ></motion.div>
+        </div>
       </div>
-    </div>
+      <h4 id={styles.title} className="paragraph">
+        Attendance
+      </h4>
+    </>
   );
 }
 
