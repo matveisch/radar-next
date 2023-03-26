22ac40cf798aa8fb94cd028111defdddeead9447
Matvei <matvesch@protonmail.com>
2023-03-26 09:30:14 +0300

    basic rtl support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,17 @@ import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import '../styles/globals.scss';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function App({ Component, pageProps }: AppProps) {
+  const { locale } = useRouter();
+  const dir = locale === 'he' ? 'rtl' : 'ltr';
+
+  useEffect(() => {
+    document.documentElement.dir = dir;
+  }, [dir]);
+
   return (
     <Layout>
       <Component {...pageProps} />
