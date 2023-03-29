b573269ceabd7eae18a67bd74b69f118dd10ad55
Matvei <matvesch@protonmail.com>
2023-03-29 17:33:33 +0300

    about page translation

--- pages/about-us/index.tsx
+++ pages/about-us/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './index.module.scss';
 import AboutCard from '../../components/aboutCard';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
@@ -10,47 +10,43 @@ import ReputationMeter from '../../components/reputation-meter/ReputationMeter';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper';
 import 'swiper/css';
-import { start } from 'repl';
-const members = [
-  {
-    job: 'Chief Executive Leader',
-    name: 'Семён Шварцман',
-    textArr: ['5 лет опыт организации рекламной деятельности', 'Эксперт в сфере контакта с инфлюенсерами'],
-    img: 'simon.jpg',
-  },
-  {
-    job: 'Executive Leader',
-    name: 'Рина Журавлева',
-    textArr: [
-      'Опыт работы в рекламном бизнесе  более 25 лет',
-      'Более 10 лет возглавляла Русскую Медиа Группу в Санкт-Петербурге (холдинг радиостанций «Русское радио», «Максимум», «Ди-фм», «Монте-Карло», «Питер-Фм»)',
-      'Более 10 лет руководит рекламным агентством полного цикла',
-      'Эксперт по продажам, дипломированный психолог',
-    ],
-    img: 'rina.jpg',
-  },
-  {
-    job: 'Development Executive Leader',
-    name: 'Вадим Снегирев',
-    textArr: [
-      'Опыт работы в рекламном бизнесе более 20 лет',
-      'Эксперт в продвижении рекламных агентств на рынке России',
-    ],
-    img: 'vadim.jpg',
-  },
-  {
-    job: 'General Chief Staff Leader',
-    name: 'Елена Крахмаль',
-    textArr: [
-      'Более 20 лет опта работы в рекламном бизнесе',
-      'Более тысячи успешных кейсов свидетельствуют об экспертности в коммуникации с держателями ресурсов',
-    ],
-    img: 'elena.jpg',
-  },
-];
+import { useTranslation } from 'next-i18next';
 
 const AboutUs = () => {
+  const { t } = useTranslation('about');
   // const [count, setCount] = useState(0);
+
+  const members = [
+    {
+      job: 'Chief Executive Leader',
+      name: t('simonName'),
+      textArr: [t('simonDescription.experience'), t('simonDescription.expert')],
+      img: 'simon.jpg',
+    },
+    {
+      job: 'Executive Leader',
+      name: t('rinaName'),
+      textArr: [
+        t('rinaDescription.experience'),
+        t('rinaDescription.spb'),
+        t('rinaDescription.marketingAgency'),
+        t('rinaDescription.expert'),
+      ],
+      img: 'rina.jpg',
+    },
+    {
+      job: 'Development Executive Leader',
+      name: t('vadimName'),
+      textArr: [t('vadimDescription.experience'), t('vadimDescription.expert')],
+      img: 'vadim.jpg',
+    },
+    {
+      job: 'General Chief Staff Leader',
+      name: t('elenaName'),
+      textArr: [t('elenaDescription.experience'), t('elenaDescription.cases')],
+      img: 'elena.jpg',
+    },
+  ];
 
   // useEffect(() => {
   //   console.log(count);
@@ -66,21 +62,15 @@ const AboutUs = () => {
   return (
     <div>
       <Head>
-        <title>About Us</title>
+        <title>{t('aboutUs')}</title>
         <meta name="viewport" content="width=device-width, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div id={styles.main}>
-        <h2>About us</h2>
+        <h2>{t('aboutUs')}</h2>
         <div id={styles.topFlex}>
-          <p>
-            Radar Digitaly - маркетинговая компания, которая помогает бизнесам достигать своих целей и увеличивать
-            прибыль. Наша команда экспертов в области маркетинга и рекламы создает индивидуальные стратегии для каждого
-            клиента, учитывая его потребности и особенности бизнеса. Мы используем современные технологии и инструменты,
-            чтобы максимально эффективно достигать поставленных целей. Мы гордимся своими успехами и стараемся делать
-            все возможное, чтобы наши клиенты чувствовали себя уверенно в своем успехе.
-          </p>
-          <div id={styles.meterWrapper}>
+          <p>{t('aboutRadar')}</p>
+          <div id={styles.meterWrapper} dir={'ltr'}>
             <Swiper
               slidesPerView={1}
               className="mySwiper"
@@ -108,7 +98,7 @@ const AboutUs = () => {
           </div>
         </div>
 
-        <h2 className="H1">Team</h2>
+        <h2 className="H1">{t('team')}</h2>
         <div id={styles.membersWrapper}>
           {members.map((item, index) => {
             return (
@@ -133,7 +123,7 @@ export default AboutUs;
 export async function getStaticProps({ locale }: { locale: string }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['contact', 'header', 'services'])),
+      ...(await serverSideTranslations(locale, ['about', 'header', 'services', 'common'])),
     },
   };
 }
