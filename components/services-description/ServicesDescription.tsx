7262377d625330b104c508dc5c2d5d753a1a0dc7
Matvei <matvesch@protonmail.com>
2023-04-07 08:54:30 +0300

    added key

--- components/services-description/ServicesDescription.tsx
+++ components/services-description/ServicesDescription.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import styles from './ServicesDescription.module.scss';
 import TariffCard from '../../ui/serviceTariff';
