cfaad53e7674f0de1576840e17a023a9d25a4d99
Matvei <matvesch@protonmail.com>
2023-04-14 09:16:19 +0300

    fixes

--- ui/language-toggle-mobile/index.tsx
+++ ui/language-toggle-mobile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './index.module.scss';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
